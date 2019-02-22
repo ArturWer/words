515737f99163723ff18417f8a2f268417e863720
Arthur Vershyhora <ArturiTunes@gmail.com>
2019-02-22 10:42:56 +0200

    Added inserting SPACE between symbols

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,15 @@ let userWord = document.getElementById('userWord');
 let main = document.querySelector("main");
 let msg = document.querySelector(".msg");
 
-userWord.addEventListener("keypress", ()=>{
+function addSymbols(words){
+	let trimWords = words.trim();
+	let wordArr = trimWords.split("");
+	let newString = wordArr.join(" ");
+	return newString;
+};
+
+userWord.addEventListener("keydown", ()=>{
 	let userText = userWord.value;
+	userText = addSymbols(userText);
 	msg.textContent = userText;
-	console.log(userText);
 });
